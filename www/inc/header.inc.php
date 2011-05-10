ca2d0137d2acc4cec91201afbf7b2426dd370470
Andrea Piccinelli <frasten@gmail.com>
2011-05-10 17:12:42 +0200

    Inserito del codice di intestazione/footer del sito originale.
    Signed-off-by: Andrea Piccinelli <frasten@gmail.com>

---
+++ www/inc/header.inc.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it" dir="ltr">
+<head>
+<title>Database and Information System Group</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="MSSmartTagsPreventParsing" content="TRUE" />
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-2553857-10");
+pageTracker._initData();
+pageTracker._trackPageview();
+</script>
+<link href="css/stile.css" rel="stylesheet" type="text/css">
+<!--[if IE]>
+<link href="css/stileIE.css" rel="stylesheet" type="text/css">
+<![endif]-->
+<link href="css/stileENG.css" rel="stylesheet" type="text/css"><LINK REL="shortcut icon" HREF="http://www.kweepy.it/unibs.ico"><script type="text/javascript" src="javascript/onfocus.js"></script>
+<script type="text/javascript" src="javascript/button.js"></script>
+</head>
+
+<body class="corsi" id="body">
+<div id="container">
+
+<div id="intestazione">
+	&nbsp;
+</div>
+
+<div id="menu">
+<ul class="menuNavigazione">
+	<li><a id="home" href="home.php">Home</a></li>
+	<li><a id="team" href="team.php" class="continua">Team</a>
+		<ul>
+			<li><a href="vda.php">Valeria De Antonellis</a></li>
+			<li><a href="bianchin.php">Devis Bianchini</a></li>
+			<li><a href="melchior.php">Michele Melchiori</a></li>
+			<li><a href="salvi.php">Denise Salvi</a></li>
+		</ul>
+	</li>
+	<li><a id="progetti" href="progetti.php">Research Projects</a></li>
+	<li><a id="pubblicazioni" href="pubblicazioni.php">Publications</a></li>
+	<li id="long"><a id="corsi" href="didattica.php" class="continua">Teaching (ita)</a>
+		<ul>
+			<li><a href="corso.php?siNO">Sistemi informativi (N.O.) - Universit&agrave; di Brescia</a></li>
+			<li><a href="corso.php?sir">Sistemi informativi in rete (N.O.) - Universit&agrave; di Brescia</a></li>
+			<li><a href="corso.php?bd">Basi di dati (N.O.) - Universit&agrave; di Brescia</a></li>
+			<li><a href="corso.php?bde">Basi di dati evolute (N.O.) - Universit&agrave; di Brescia</a></li>
+			<li><a href="corso.php?siVO">Sistemi informativi (V.O.) - Universit&agrave; di Brescia</a></li>
+			<li><a href="corso.php?fib">Fondamenti di Informatica B (N.O.) - Universit&agrave; di Brescia</a></li>
+			<li><a href="corso.php?id">Informatica per Disegno Industriale - Universit&agrave; di Brescia</a></li>
+			<li><a href="corso.php?siPOLI">Sistemi informativi - Politecnico di Milano</a></li>
+			<li><a href="corso.php?pw">Programmazione Web - Universit&agrave; di Brescia</a></li>
+		</ul>
+	</li>
+	<li><a id="tesi" href="tesi.php">Thesis (ita)</a></li>
+</ul>
+</div>
+
+<div id="flags">
+	<a href="/~deantone/corso.php?ita" title="Vai al sito italiano!"><div id="linguaITA">&nbsp;</div></a>
+	<a href="/~deantone/corso.php?eng" title="Go to English site!"><div id="linguaENG">&nbsp;</div></a>
+</div>
+
+<div id="centrale">
+	<div id="corpo">
---
+++ www/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once('inc/header.inc.php');
+
+echo "CONTENUTO";
+
+require_once('inc/footer.inc.php');
+
+
+?>
