19e2759819d65dd7c81b504bd5f27337b01f9543
Andrea Piccinelli <frasten@gmail.com>
2011-08-03 11:11:25 +0200

    Abbozzato funzionamento lista di pubblicazioni
    Signed-off-by: Andrea Piccinelli <frasten@gmail.com>

--- www/inc/pub-list.inc.php
+++ www/inc/pub-list.inc.php
@@ -1,6 +1,33 @@
 <h1>Pubblicazioni</h1>
 <?php
 
+$query = <<<EOF
+SELECT * FROM
+	`$config[db_prefix]pubblicazione`
+	LEFT JOIN `$config[db_prefix]pubblicazione_pubautore`
+		ON `$config[db_prefix]pubblicazione`.`id` = `$config[db_prefix]pubblicazione_pubautore`.`id_pubblicazione`
+	LEFT JOIN `$config[db_prefix]pubautore`
+		ON `$config[db_prefix]pubblicazione_pubautore`.`id_autore` = `$config[db_prefix]pubautore`.`id`
+ORDER BY `$config[db_prefix]pubblicazione`.`anno` DESC
+EOF;
 
+$result = mysql_query( $query, $db );
+if ( ! $result ) {
+	echo "Errore interno.";
+	return 1;
+}
+
+if ( mysql_num_rows( $result ) == 0 ) {
+	echo "Non &egrave; presente alcuna pubblicazione.";
+}
+else {
+	echo "<ul>\n";
+	while ( $riga = mysql_fetch_assoc( $result ) ) {
+		echo "<li>\n";
+		print_r($riga);
+		echo "</li>\n";
+	}
+	echo "</ul>\n";
+}
 
 ?>
