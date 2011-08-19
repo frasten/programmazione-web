8054e1e53b531193e0f5ebd18e0399aadb6cbe04
Andrea Piccinelli <frasten@gmail.com>
2011-08-19 12:11:00 +0200

    Aggiunta la visualizzazione dei materiali nella pagina dei corsi.
    Signed-off-by: Andrea Piccinelli <frasten@gmail.com>

--- www/inc/corso-singolo.inc.php
+++ www/inc/corso-singolo.inc.php
@@ -106,5 +106,87 @@ echo $corso['orario'];
 <?php endif; ?>
 
 
+<?php
+	$query = <<<EOF
+SELECT
+	s.`id_sezione`,
+	s.`note`,
+	s.`titolo` AS titolo_sez,
+	f.`titolo` AS titolo_file,
+	`aggiornato`,
+	f.`url`
+FROM `$config[db_prefix]sezione` AS s
+JOIN `$config[db_prefix]file_materiale` AS f
+	USING (`id_sezione`)
+WHERE `id_corso` = '$id' AND `nascondi` = '0'
+ORDER BY `id_sezione` ASC, `ordine` ASC
+EOF;
+	$result = mysql_query( $query, $db );
+
+	if ( mysql_num_rows( $result ) ):
+?>
 <h3>Materiale didattico</h3>
-TODO
+(<?php updated_icon() ?> = aggiornati per l'anno accademico in corso)
+<?php
+	$oldsez = false;
+	while ( $riga = mysql_fetch_assoc( $result ) ) {
+		if ( $oldsez != $riga['id_sezione'] ) {
+			if ( $oldsez !== false ) echo "</ul>\n";
+			printf( "<h4>%s</h4>\n", htmlspecialchars( $riga['titolo_sez'] ) );
+			if ( trim( strip_tags( $riga['note'] ) ) )
+				echo $riga['note'];
+
+			echo "<ul>\n";
+			$oldsez = $riga['id_sezione'];
+		}
+		echo "<li>\n";
+
+		$url = $riga['url'];
+		if ( ! preg_match( "#^(?:http|https|ftp?)://#i", $url ) ) {
+			// Url relativo, caricato attraverso form
+			$url = basename( $url ); // per sicurezza eliminiamo sottodirectory
+			$url = "http://$_SERVER[SERVER_NAME]" . dirname( $_SERVER['SCRIPT_NAME'] ) . "/$config[upload_path]$url";
+		}
+
+		$file = basename( $url );
+		preg_match( "/([^.]+)$/", $url, $match );
+		$estensione = $match[1];
+
+		switch ( strtolower( $estensione ) ) {
+			case 'pdf':
+				$img = 'icon_pdf.png';
+				break;
+			case 'zip':
+			case 'rar':
+			case 'bz2':
+			case 'gz':
+				$img = 'icon_zip.png';
+				break;
+			case 'doc':
+			case 'odt':
+			case 'docx':
+				$img = 'icon_doc.png';
+				break;
+			default:
+				$img = 'software.gif';
+				break;
+		}
+
+		printf( "<a href='%s' class='iconalink' title='Scarica il file'><img src='img/icone/%s' alt='Scarica il file' /></a>\n",
+			htmlspecialchars( $url, ENT_QUOTES ),
+			$img );
+		echo htmlspecialchars( $riga['titolo_file'] ) . "\n";
+		if ( $riga['aggiornato'] )
+			updated_icon();
+		echo "</li>\n";
+	}
+	echo "</ul>\n";
+endif;
+
+
+
+function updated_icon() {
+	echo "<img src='img/icone/update.gif' alt='***Updated!***' />\n";
+}
+
+?>
